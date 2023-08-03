cd6d00b51763f29c776a59ba6121c4ffe864be42
Rockojoe2 <lee.d.joey@gmail.com>
2023-08-03 17:53:07 -0500

    Made the three tables department, roles, and employees

---
+++ index.js
@@ -0,0 +1,37 @@
+/*Pseudocode
+
+We want to choose what we want to do in the beginning.
+Give employees a list of options on what they want to do and with the options, it will lead down certain paths.
+Options will be 
+1. view all departments
+2. view all roles
+3. view all employees
+4. add a department 
+5.add a role
+6. add an employee
+7. update an employee role
+
+First thing we have to do, is create the company databases
+
+1. View All Departments
+We see a SQL table of all the departments. We're going to have to create a department table
+
+2. View all roles
+We see a sql table of every role that is available. We're going to have to create a roles table
+
+3. View all employees
+We see a sql table of every employee in the company. We're going to have to create an employee table.
+
+4. Add a department
+add a department to the department table. So add a department to the department table
+
+5. Add a role
+We're going to have to add a role to the role table
+
+ 6. Add an employee
+We're going to have to add employee information to the employee table
+
+7. Update an employee role
+Edit the employee table to fix the employee role.
+
+*/
